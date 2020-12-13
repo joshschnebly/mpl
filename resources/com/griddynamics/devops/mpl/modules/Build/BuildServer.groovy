37c3adf36610e0b3ec975f7574aa8a77f2434704
Josh <josh.schnebly@gmail.com>
2020-12-13 17:15:10 -0600

    asdfg

--- resources/com/griddynamics/devops/mpl/modules/Build/BuildServer.groovy
+++ resources/com/griddynamics/devops/mpl/modules/Build/BuildServer.groovy
@@ -1,4 +1,4 @@
 bat "dotnet clean ${CFG.'solution_filename'} -c ${CFG.'configuration_property'} -r ${CFG.'runtime_identifier'} -v minimal"
 
-def outputDirectory = CFG.'current_release_number' != null ? "${CFG.'output_directory'}.${CFG.'current_release_number'}" : CFG.'output_directory' 
+def outputDirectory = CFG.'current_release_number' != null ? "${CFG.'output_directory'}.${CFG.'current_release_number'}" : "${CFG.'output_directory'}" 
 bat "dotnet publish ${CFG.'projectfile_path'} -c ${CFG.'configuration_property'} -o ${CFG.'output_directory'} -r ${CFG.'runtime_identifier'} --self-contained true /p:UseAppHost=true" 
