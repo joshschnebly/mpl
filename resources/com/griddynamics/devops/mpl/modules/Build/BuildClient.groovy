2509fd912036dd017d5a6dd227ac2663863b9858
Josh <josh.schnebly@gmail.com>
2020-12-14 11:43:32 -0600

    sadf

--- resources/com/griddynamics/devops/mpl/modules/Build/BuildClient.groovy
+++ resources/com/griddynamics/devops/mpl/modules/Build/BuildClient.groovy
@@ -1,6 +1,7 @@
 def packageManager = CFG.'package_manager'
 if(packageManager == 'npm' || packageManager == 'yarn') {
-  bat(label: "Build NPM Packages via ${packageManager}", script: "cd ${CFG.'client_folder'} & ${packageManager} run build") 
+  def cdString = CFG.'client_folder' != null ? "cd ${CFG.'client_folder'} & " : ""
+  bat(label: "Build NPM Packages via ${packageManager}", script: "${cdString} ${packageManager} run build") 
 }
 else {
   echo 'Error - MPL Build Client- invalid package manager'
--- resources/com/griddynamics/devops/mpl/modules/Restore/RestoreClient.groovy
+++ resources/com/griddynamics/devops/mpl/modules/Restore/RestoreClient.groovy
@@ -1,6 +1,8 @@
 def packageManager = CFG.'package_manager'
+
 if(packageManager == 'npm' || packageManager == 'yarn') {
-  bat(label: "Restore NPM Packages via ${packageManager}", script: "cd ${CFG.'client_folder'} & ${packageManager} install") 
+  def cdString = CFG.'client_folder' != null ? "cd ${CFG.'client_folder'} & " : ""
+  bat(label: "Restore NPM Packages via ${packageManager}", script: "${cdString} ${packageManager} install") 
 }
 else {
   echo 'Error - MPL Restore Client- invalid package manager'
