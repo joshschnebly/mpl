db26e2c3d59311ecff26b74dd7f10fbc6867119c
Josh <josh.schnebly@gmail.com>
2020-12-14 13:25:21 -0600

    sdfg

--- resources/com/griddynamics/devops/mpl/modules/Startup/Startup.groovy
+++ resources/com/griddynamics/devops/mpl/modules/Startup/Startup.groovy
@@ -1,7 +1,7 @@
 scmSkip(deleteBuild: true, skipPattern:'.*\\[ci skip\\].*')
 currentBuild.displayName = "${BRANCH_NAME}-${currentBuild.id}"
 bat(label: 'Current Build', script: "echo Current Build : ${currentBuild.displayName}")
-
+echo "s-${CFG.'project_folder'}
 if(CFG.'previous_release_number' != null){
   OUT.'returns.previous_release_number' =  MPLModule('Startup Semantic').'returns.previous_release_number'
 }
--- resources/com/griddynamics/devops/mpl/modules/Startup/StartupSemantic.groovy
+++ resources/com/griddynamics/devops/mpl/modules/Startup/StartupSemantic.groovy
@@ -1,3 +1,4 @@
+echo "ss-${CFG.'project_folder'}
 OUT.'common.previous_release_number' = readJSON(file: "${CFG.'project_folder'}\\package.json").version
 bat(label: 'Previous Release', script: "echo ${OUT.'common.previous_release_number'}")
 
