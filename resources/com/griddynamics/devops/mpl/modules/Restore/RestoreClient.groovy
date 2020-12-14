36bc32394003861f68870815cf2df6c98caadd21
Josh <josh.schnebly@gmail.com>
2020-12-14 11:56:49 -0600

    asdf

--- resources/com/griddynamics/devops/mpl/modules/Restore/RestoreClient.groovy
+++ resources/com/griddynamics/devops/mpl/modules/Restore/RestoreClient.groovy
@@ -2,6 +2,7 @@ def packageManager = CFG.'package_manager'
 
 if(packageManager == 'npm' || packageManager == 'yarn') {
   def cdString = CFG.'client_folder' != null ? "cd ${CFG.'client_folder'} & " : ""
+  echo "-${cdString} ${packageManager} install-"
   bat(label: "Restore NPM Packages via ${packageManager}", script: "${cdString} ${packageManager} install") 
 }
 else {
