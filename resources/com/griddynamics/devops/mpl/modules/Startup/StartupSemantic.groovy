a45c5c8393a788bc041bd43bc14b3857ffbc910c
Josh <josh.schnebly@gmail.com>
2020-12-14 13:28:18 -0600

    asdf

--- resources/com/griddynamics/devops/mpl/modules/Startup/StartupSemantic.groovy
+++ resources/com/griddynamics/devops/mpl/modules/Startup/StartupSemantic.groovy
@@ -1,4 +1,5 @@
 echo "ss-${CFG.'project_folder'}"
+echo "ss-${CFG.toString()}"
 OUT.'common.previous_release_number' = readJSON(file: "${CFG.'project_folder'}\\package.json").version
 bat(label: 'Previous Release', script: "echo ${OUT.'common.previous_release_number'}")
 
