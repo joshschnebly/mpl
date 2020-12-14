ce68eeee0c594285c19c4949979d11dd55b14626
Josh <josh.schnebly@gmail.com>
2020-12-14 12:05:38 -0600

    asdf

--- resources/com/griddynamics/devops/mpl/modules/Build/BuildClient.groovy
+++ resources/com/griddynamics/devops/mpl/modules/Build/BuildClient.groovy
@@ -1,8 +1,9 @@
 def packageManager = CFG.'package_manager'
+
 if(packageManager == 'npm' || packageManager == 'yarn') {
   def cdString = CFG.'client_folder' != null ? "cd ${CFG.'client_folder'} & " : ""
   bat(label: "Build NPM Packages via ${packageManager}", script: "${cdString} ${packageManager} run build") 
 }
 else {
-  echo 'Error - MPL Build Client- invalid package manager'
+  echo "Error - MPL Build Client- invalid package manager: ${packageManager}"
 }
--- resources/com/griddynamics/devops/mpl/modules/Restore/RestoreClient.groovy
+++ resources/com/griddynamics/devops/mpl/modules/Restore/RestoreClient.groovy
@@ -2,11 +2,10 @@ def packageManager = CFG.'package_manager'
 
 if(packageManager == 'npm' || packageManager == 'yarn') {
   def cdString = CFG.'client_folder' != null ? "cd ${CFG.'client_folder'} & " : ""
-  echo "-${cdString} ${packageManager} install-"
   bat(label: "Restore NPM Packages via ${packageManager}", script: "${cdString} ${packageManager} install") 
 }
 else {
-  echo 'Error - MPL Restore Client- invalid package manager'
+  echo "Error - MPL Restore Client- invalid package manager: ${packageManager}"
 }
 
 
