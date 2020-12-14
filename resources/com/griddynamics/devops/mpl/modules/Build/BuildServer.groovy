5f6737d556473bea61fea556bbc8c230dfdc8043
Josh <josh.schnebly@gmail.com>
2020-12-13 19:20:22 -0600

    asdf

--- resources/com/griddynamics/devops/mpl/modules/Build/BuildServer.groovy
+++ resources/com/griddynamics/devops/mpl/modules/Build/BuildServer.groovy
@@ -1,5 +1,2 @@
 bat "dotnet clean ${CFG.'solution_filename'} -c ${CFG.'configuration_property'} -r ${CFG.'runtime_identifier'} -v minimal"
-
-echo "CRN-${CFG.'current_release_number'}"
-def outputDirectory = CFG.'current_release_number' != null ? "${CFG.'output_directory'}.${CFG.'current_release_number'}" : "${CFG.'output_directory'}" 
-bat "dotnet publish ${CFG.'projectfile_path'} -c ${CFG.'configuration_property'} -o ${outputDirectory} -r ${CFG.'runtime_identifier'} --self-contained true /p:UseAppHost=true" 
+bat "dotnet publish ${CFG.'projectfile_path'} -c ${CFG.'configuration_property'} -o "${CFG.'output_directory'}"  -r ${CFG.'runtime_identifier'} --self-contained true /p:UseAppHost=true" 
---
+++ vars/Robocopy.groovy
@@ -0,0 +1,10 @@
+def robocopy(String cmd)
+{
+	// robocopy uses non-zero exit code even on success, status greater than 3 is fine
+	def status = bat returnStatus: true, script: "ROBOCOPY ${cmd} /nfl /ndl /np"
+	echo "ROBOCOPY returned ${status}"
+	if (status < 0 || status > 3)
+	{
+		error("ROBOCOPY failed")
+	}
+}
