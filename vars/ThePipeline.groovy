26e5de7816789ff25b861e251bf44e29c559808a
Josh <josh.schnebly@gmail.com>
2020-12-14 11:13:06 -0600

    asdf

--- vars/ThePipeline.groovy
+++ vars/ThePipeline.groovy
@@ -91,6 +91,7 @@ def call(body) {
         }
       }
       stage( 'Restore' ) {
+        when { expression { MPLModuleEnabled() } }
         parallel {    
            stage( 'Restore Server' ) {
             when { expression { MPLModuleEnabled() } }
