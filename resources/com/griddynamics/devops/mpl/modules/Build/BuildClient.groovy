95c2ae18d8c48a34d7d21cd68aeedd2a221bcf1c
Josh <josh.schnebly@gmail.com>
2020-12-13 18:18:07 -0600

    asdf

--- resources/com/griddynamics/devops/mpl/modules/Build/BuildClient.groovy
+++ resources/com/griddynamics/devops/mpl/modules/Build/BuildClient.groovy
@@ -1,9 +1,7 @@
-bat "cd ${ClientSourceDirectory} yarn run build"
-
 def packageManager = CFG.'package_manager'
 if(packageManager == 'npm' || packageManager == 'yarn') {
-  bat(label: "Restore NPM Packages via ${packageManager}", script: "cd ${CFG.'restore_folder'} & ${packageManager} run build") 
+  bat(label: "Build NPM Packages via ${packageManager}", script: "cd ${CFG.'restore_folder'} & ${packageManager} run build") 
 }
 else {
-  echo 'Error - MPL Restore Client- invalid package manager'
+  echo 'Error - MPL Build Client- invalid package manager'
 }
