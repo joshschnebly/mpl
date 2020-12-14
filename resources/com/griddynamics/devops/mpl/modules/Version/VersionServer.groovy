2da932b5455e4075abcc4a8ed487e006b1e4f46c
Josh <josh.schnebly@gmail.com>
2020-12-13 18:00:57 -0600

    asdf

--- resources/com/griddynamics/devops/mpl/modules/Version/VersionServer.groovy
+++ resources/com/griddynamics/devops/mpl/modules/Version/VersionServer.groovy
@@ -3,4 +3,4 @@ if (CFG.'current_release_number' == null) {
     OUT.'returns.current_release_number' = VersionNumber([projectStartDate: '2020-01-01', versionNumberString: '1.${BUILD_MONTH}.${BUILD_DAY}.${BUILDS_TODAY}'])  
 }
 echo "ReleaseNumber: ${OUT.'returns.current_release_number'}"
-changeAsmVer(regexPattern: 'Assembly(\\w*)Version\\("([0-9]+)\\.([0-9]+)\\.([0-9]+)\\.([0-9]+)"\\)', replacementPattern: 'Assembly$1Version("%s")', versionPattern: OUT.'shared.current_release_number')
+changeAsmVer(regexPattern: 'Assembly(\\w*)Version\\("([0-9]+)\\.([0-9]+)\\.([0-9]+)\\.([0-9]+)"\\)', replacementPattern: 'Assembly$1Version("%s")', versionPattern: OUT.'returns.current_release_number')
