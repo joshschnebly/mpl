3ea00c9c0c121dd7c78898c044f080cb4eef1702
Josh <josh.schnebly@gmail.com>
2020-12-15 08:40:56 -0600

    asdf

--- resources/com/griddynamics/devops/mpl/modules/Startup/StartupSemantic.groovy
+++ resources/com/griddynamics/devops/mpl/modules/Startup/StartupSemantic.groovy
@@ -1,4 +1,4 @@
-OUT.'shared.previous_version_number' = readJSON(file: "${CFG.'project_folder'}\\package.json").version
-bat(label: 'Previous Release', script: "echo ${OUT.'shared.previous_version_number'}")
+OUT.'returns.previous_version_number' = readJSON(file: "${CFG.'project_folder'}\\package.json").version
+bat(label: 'Previous Release', script: "echo ${OUT.'returns.previous_version_number'}")
 
 
