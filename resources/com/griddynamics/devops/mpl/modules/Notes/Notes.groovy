4f038a450b03a32e3dfe7e5a7d71c9eb63c03a68
Josh <josh.schnebly@gmail.com>
2020-12-15 12:40:55 -0600

    Sdf

--- resources/com/griddynamics/devops/mpl/modules/Notes/Notes.groovy
+++ resources/com/griddynamics/devops/mpl/modules/Notes/Notes.groovy
@@ -1,9 +1,16 @@
-def releaseNotesPath = ${CFG.'project_folder'} ? ".\\${CFG.'project_folder'}\\RELEASE_NOTES.md": "RELEASE_NOTES.md"
+def releaseNotesPath = 'RELEASE_NOTES.md'
+def projectFolder = ''
+
+if(CFG.'project_folder' != null)
+{
+    releaseNotesPath = ".\\${CFG.'project_folder'}\\RELEASE_NOTES.md"
+    projectFolderCd = "cd ${CFG.'project_folder'} & "
+}
 
 bat(label: 'Clear release notes', script: "if exist ${releaseNotesPath} del ${releaseNotesPath}")
 env.GIT_REPOSITORY_URL = CFG.'git_repository_url'
 
 withCredentials([usernamePassword(credentialsId: "${CFG.'jenkins_ghe_token'}", usernameVariable: 'GITHUB_USERNAME', passwordVariable: 'GITHUB_TOKEN')]) {
-    bat(label: 'Generating Semantic Release Notes', script: "cd ${CFG.'project_folder'} & npx semantic-release --branches ${env.BRANCH_NAME}")
+    bat(label: 'Generating Semantic Release Notes', script: "${projectFolderCd} npx semantic-release --branches ${env.BRANCH_NAME}")
 }
 
