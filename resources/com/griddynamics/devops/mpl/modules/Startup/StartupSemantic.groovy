a099aedf8415a74051726a60e15a3a70f293dd2a
Josh <josh.schnebly@gmail.com>
2020-12-15 12:53:10 -0600

    drfy

--- resources/com/griddynamics/devops/mpl/modules/Startup/StartupSemantic.groovy
+++ resources/com/griddynamics/devops/mpl/modules/Startup/StartupSemantic.groovy
@@ -1,4 +1,4 @@
-def packageJsonPath = (${CFG.'project_folder'} ? ".") + "\\package.json"
+def packageJsonPath = (${CFG.'project_folder'} ?: ".") + "\\package.json"
 OUT.'returns.previous_version_number' = readJSON(file: "${packageJsonPath}").version
 bat(label: 'Previous Release', script: "echo ${OUT.'returns.previous_version_number'}")
 
