e0164877e615cdb4219df7a3eae2fef1b94d8459
Josh <josh.schnebly@gmail.com>
2020-12-15 10:50:45 -0600

    asdf

--- resources/com/griddynamics/devops/mpl/modules/Build/BuildClient.groovy
+++ resources/com/griddynamics/devops/mpl/modules/Build/BuildClient.groovy
@@ -2,7 +2,7 @@ def packageManager = CFG.'package_manager'
 
 if(packageManager == 'npm' || packageManager == 'yarn') {
   def clientFolder = CFG.'client_folder' != null ? "cd ${CFG.'client_folder'} & " : ""
-  def postRunBuildArgs = CFG.'post_run_build_args' != null ? ":${CFG.'post_run_build_args'}" : ""
+  def postRunBuildArgs = CFG.'post_run_build_args' != null ? "${CFG.'post_run_build_args'}" : ""
   
   bat(label: "Build Client via ${packageManager}", script: "${clientFolder} ${packageManager} run build${postRunBuildArgs}") 
 }
