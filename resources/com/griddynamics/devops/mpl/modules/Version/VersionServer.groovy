a714e3db81d3c35f975da98b5af705a8563f9b2a
Josh <josh.schnebly@gmail.com>
2020-12-15 13:06:35 -0600

    sdfg

--- resources/com/griddynamics/devops/mpl/modules/Version/VersionServer.groovy
+++ resources/com/griddynamics/devops/mpl/modules/Version/VersionServer.groovy
@@ -5,7 +5,8 @@ if (CFG.'current_version_number' == null) {
 }
 else {
   //Semantic versioning
-  OUT.'returns.current_version_number' = readJSON(file: "${CFG.'project_folder'}\\package.json").version
+  def packageJsonPath = (CFG.'project_folder' ?: ".") + "\\package.json"
+  OUT.'returns.current_version_number' = readJSON(file: ${packageJsonPath}).version
   changeAsmVer(regexPattern: 'Assembly(\\w*)Version\\("([0-9]+)\\.([0-9]+)\\.([0-9]+)\\.(\\*)"\\)', replacementPattern: 'Assembly$1Version("%s")', versionPattern: "${OUT.'returns.current_version_number'}.*")
 }
                 
