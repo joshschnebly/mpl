3135676472f14cf1fdf9450867b162b190bfa2db
Josh <josh.schnebly@gmail.com>
2020-12-16 15:24:41 -0600

    asdf

--- resources/com/griddynamics/devops/mpl/modules/Build/BuildServerWebMsBuild.groovy
+++ resources/com/griddynamics/devops/mpl/modules/Build/BuildServerWebMsBuild.groovy
@@ -1,7 +1,7 @@
 def configurationPropery = "${CFG.'configuration_property'}"
 def packageVersionValue = "${CFG.'current_version_number'}"
-def webProjectName = "${CFG.'web_project_name'}"
-def projectPath = "${webProjectName}\\${webProjectName}.csproj"
+def projectName = "${CFG.'project_name'}"
+def projectPath = "${projectName}\\${projectName}.csproj"
 def deploymentDirectory = "${WORKSPACE}\\Deployment"
 
-bat(label: "Build and Package ${webProjectName}.csproj", script: "\"${tool 'Visual Studio 2019'}\\MSBuild.exe\" ${projectPath} /p:Configuration=${configurationPropery};Platform=\"AnyCPU\";ReleaseVersion=\"${packageVersionValue}\";PackageVersion=\"${packageVersionValue}\";publishUrl=\"${deploymentDirectory}\";WebPublishMethod=FileSystem /t:WebPublish /p:VisualStudioVersion=15.0")
+bat(label: "Build and Package ${projectName}.csproj", script: "\"${tool 'Visual Studio 2019'}\\MSBuild.exe\" ${projectPath} /p:Configuration=${configurationPropery};Platform=\"AnyCPU\";ReleaseVersion=\"${packageVersionValue}\";PackageVersion=\"${packageVersionValue}\";publishUrl=\"${deploymentDirectory}\";WebPublishMethod=FileSystem /t:WebPublish /p:VisualStudioVersion=15.0")
--- resources/com/griddynamics/devops/mpl/modules/Build/BuildServerWebPublish.groovy
+++ resources/com/griddynamics/devops/mpl/modules/Build/BuildServerWebPublish.groovy
@@ -1,3 +1,2 @@
-echo "outputd-BuildServerWebPublish:${CFG.'output_directory'}"
 bat "dotnet clean ${CFG.'solution_filename'} -c ${CFG.'configuration_property'} -r ${CFG.'runtime_identifier'} -v minimal"
 bat "dotnet publish ${CFG.'projectfile_path'} -c ${CFG.'configuration_property'} -o ${CFG.'output_directory'} -r ${CFG.'runtime_identifier'} --self-contained true /p:UseAppHost=true" 
