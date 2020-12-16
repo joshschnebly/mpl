dd52d13c2dac9c3ecaa694e91f4600f44b69406a
Josh <josh.schnebly@gmail.com>
2020-12-15 20:13:09 -0600

    asdf

--- resources/com/griddynamics/devops/mpl/modules/Version/VersionServer.groovy
+++ resources/com/griddynamics/devops/mpl/modules/Version/VersionServer.groovy
@@ -1,15 +1,17 @@
+def currentVersionNumber = ''
 if (CFG.'current_version_number' == null) {
     //Default versioning 
-  OUT.'returns.current_version_number' = VersionNumber([projectStartDate: '2020-01-01', versionNumberString: '1.${BUILD_MONTH}.${BUILD_DAY}.${BUILDS_TODAY}'])  
-  changeAsmVer(regexPattern: 'Assembly(\\w*)Version\\("([0-9]+)\\.([0-9]+)\\.([0-9]+)\\.([0-9]+)"\\)', replacementPattern: 'Assembly$1Version("%s")', versionPattern: OUT.'returns.current_version_number')
+  currentVersionNumber = VersionNumber([projectStartDate: '2020-01-01', versionNumberString: '1.${BUILD_MONTH}.${BUILD_DAY}.${BUILDS_TODAY}'])  
+  changeAsmVer(regexPattern: 'Assembly(\\w*)Version\\("([0-9]+)\\.([0-9]+)\\.([0-9]+)\\.([0-9]+)"\\)', replacementPattern: 'Assembly$1Version("%s")', versionPattern: "${currentVersionNumber}")
 }
 else {
   //Semantic versioning
   def packageJsonPath = (CFG.'project_folder' ?: ".") + "\\package.json"
-  OUT.'returns.current_version_number' = readJSON(file: "${packageJsonPath}").version
-  changeAsmVer(regexPattern: 'Assembly(\\w*)Version\\("([0-9]+)\\.([0-9]+)\\.([0-9]+)\\.(\\*)"\\)', replacementPattern: 'Assembly$1Version("%s")', versionPattern: "${OUT.'returns.current_version_number'}.*")
+  currentVersionNumber = readJSON(file: "${packageJsonPath}").version
+  changeAsmVer(regexPattern: 'Assembly(\\w*)Version\\("([0-9]+)\\.([0-9]+)\\.([0-9]+)\\.(\\*)"\\)', replacementPattern: 'Assembly$1Version("%s")', versionPattern: "${currentVersionNumber}.*")
 }
-bat(label: 'Current Version Number', script: "echo ${OUT.'returns.current_version_number'}")
+OUT.'returns.current_version_number' = currentVersionNumber
+bat(label: 'Current Version Number', script: "echo ${currentVersionNumber}")
 
 if(CFG.'release_candidate_suffix' != null) {
   OUT.'returns.current_package_version' = "${currentVersionNumber}${CFG.'release_candidate_suffix'}"
