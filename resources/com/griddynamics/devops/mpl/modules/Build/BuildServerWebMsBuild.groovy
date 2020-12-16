c2538a9afa4b15fbdf8d7488630ad1050d15693a
Josh <josh.schnebly@gmail.com>
2020-12-16 09:41:59 -0600

    asdf

--- resources/com/griddynamics/devops/mpl/modules/Build/BuildServerWebMsBuild.groovy
+++ resources/com/griddynamics/devops/mpl/modules/Build/BuildServerWebMsBuild.groovy
@@ -1,11 +1,7 @@
 def configurationPropery = "${CFG.'configuration_property'}"
-def releaseNumber = "${CFG.'current_version_number'}"
+def packageVersionValue = "${CFG.'current_version_number'}"
 def webProjectName = "${CFG.'web_project_name'}"
-def packageVersionValue = "${releaseNumber}"
-def archiveName =  "${webProjectName}.${packageVersionValue}"
-def archivePackage = "${WORKSPACE}\\dist\\${currentBuild.id}\\${archiveName}.zip"
 def projectPath = "${webProjectName}\\${webProjectName}.csproj"
 def deploymentDirectory = "${WORKSPACE}\\Deployment"
 
 bat(label: "Build and Package ${webProjectName}.csproj", script: "\"${tool 'Visual Studio 2019'}\\MSBuild.exe\" ${projectPath} /p:Configuration=${configurationPropery};Platform=\"AnyCPU\";ReleaseVersion=\"${packageVersionValue}\";PackageVersion=\"${packageVersionValue}\";publishUrl=\"${deploymentDirectory}\";WebPublishMethod=FileSystem /t:WebPublish /p:VisualStudioVersion=15.0")
-zip(zipFile: "${archivePackage}", archive: true, dir: "${deploymentDirectory}", glob: '/**/*')
--- resources/com/griddynamics/devops/mpl/modules/Version/VersionServer.groovy
+++ resources/com/griddynamics/devops/mpl/modules/Version/VersionServer.groovy
@@ -11,14 +11,17 @@ else {
   currentVersionNumber = readJSON(file: "${packageJsonPath}").version
   changeAsmVer(regexPattern: 'Assembly(\\w*)Version\\("([0-9]+)\\.([0-9]+)\\.([0-9]+)\\.(\\*)"\\)', replacementPattern: 'Assembly$1Version("%s")', versionPattern: "${currentVersionNumber}.*")
 }
+//Current version number
 OUT.'returns.current_version_number' = currentVersionNumber
 bat(label: 'Current Version Number', script: "echo ${currentVersionNumber}")
 
+//Current package version
 if(CFG.'release_candidate_suffix' != null) {
   OUT.'returns.current_package_version' = "${currentVersionNumber}${CFG.'release_candidate_suffix'}"
   bat(label: 'Current Package Version', script: "echo ${OUT.'returns.current_package_version'}")
 }
 
+//Build Type Selected
 if(CFG.'build_type' != null) {
   bat(label: 'Build Selected', script: "echo ${CFG.'build_type'}")
 }
