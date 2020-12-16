d76fdb7ef69a82ba03b524ac3d6cce69b73f9b09
Josh <josh.schnebly@gmail.com>
2020-12-16 07:17:43 -0600

    asdf

--- vars/ThePipeline.groovy
+++ vars/ThePipeline.groovy
@@ -20,58 +20,11 @@
 // @Project:     MPL
 // @Description: Shared Jenkins Modular Pipeline Library
 //
+//echo "Before Pipeline: ${MPL.config.toString()}"
 
 def call(body) {
-  def MPL = MPLPipelineConfig(body, 
-    [:
-      /*
-      modules: [
-        Startup: [
-         project_folder: , //Needed for Semantic Startup   CashOps.Web
-        ],
-        Restore : [:],
-        //RestoreServer: [
-        //  solution_filename: '' //'CashOps.sln'
-        //],
-        //RestoreClient: [
-        //  package_manager: '',   //npm or yarn
-        //  restore_folder: '' 
-        //],
-        //Notes: [
-        //  project_name: '', //'CashOps.Web' 
-        //  jenkins_ghe_token: 'XXX',  //jenkins GHE API token 
-        //  git_repository_url: ''   //'https://h2-vspghe01.corp.lfco.cc/CMS/CashOps.git' 
-        //],
-        //Version: [
-        //  when_branch : ''  //'/^dev$|^release/.+/'
-        //],
-        //VersionServer: [:],
-        //VersionClient: [:],
-        //Build: [
-        //  when_branch : ''  //'/^dev$|^release/.+/'
-        //],
-        //BuildServer: [
-        //  project_folder : ''  //CashOps.Web
-        //],
-        //BuildClient: [:]
-      ]
-      */
-      //common : [ 
-      //  previous_version_number: null,
-      //  current_version_number: null
-      //]  
-    ]
-    ,
-    [:]
-    //[
-    //  modules: [
-    //    Notes: [
-    //      jenkins_ghe_token: 'usa_houston-svc_Octopus_BS-GHE-Token'  //jenkins GHE API token 
-    //    ] 
-    //  ]
-    //]
-  )
-  println 'Before Pipeline' + MPL.config.toString()
+  def MPL = MPLPipelineConfig(body, [:], [:])
+ 
   pipeline {
     agent any
     parameters {
@@ -174,16 +127,15 @@ def call(body) {
     }
     post {
       always {  
-        //MPLPostStepsRun('always')
+        MPLPostStepsRun('always')
         echo MPL.pipelineCode
       }
-      //success {
-        //MPLPostStepsRun('success')
-      //}
-      //failure {
-        //MPLPostStepsRun('failure')
-      //}
+      success {
+        MPLPostStepsRun('success')
+      }
+      failure {
+        MPLPostStepsRun('failure')
+      }
     }  
   }
 }
-//echo "Before Startup MPLModule: ${MPL.config.toString()}"
