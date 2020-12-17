cd6c602afe2a9e651c6641ad3aac3414a39512de
Josh <josh.schnebly@gmail.com>
2020-12-17 12:14:34 -0600

    fdg

--- resources/com/griddynamics/devops/mpl/modules/Build/BuildServer.groovy
+++ resources/com/griddynamics/devops/mpl/modules/Build/BuildServer.groovy
@@ -1,4 +1,5 @@
 CFG.'configuration_property' = CFG.'configuration_property' ?: 'release'
+echo "configuration_property:${CFG.'configuration_property'}"
 if(CFG.'web_build_type' != null)
 {
     MPLModule("Build Server${CFG.'web_build_type'}", CFG)
--- resources/com/griddynamics/devops/mpl/modules/Build/BuildServerWebPublish.groovy
+++ resources/com/griddynamics/devops/mpl/modules/Build/BuildServerWebPublish.groovy
@@ -1,2 +1,3 @@
+echo "configuration_propertyWP:${CFG.'configuration_property'}"
 bat "dotnet clean ${CFG.'solution_filename'} -c ${CFG.'configuration_property'} -r ${CFG.'runtime_identifier'} -v minimal"
 bat "dotnet publish ${CFG.'projectfile_path'} -c ${CFG.'configuration_property'} -o ${CFG.'output_directory'} -r ${CFG.'runtime_identifier'} --self-contained true /p:UseAppHost=true" 
