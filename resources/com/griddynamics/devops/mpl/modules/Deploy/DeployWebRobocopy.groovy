bc3f6d56dd285d18d3b1a614d01b44ff8ba220dd
Josh <josh.schnebly@gmail.com>
2020-12-28 16:25:16 -0600

    asdf

--- resources/com/griddynamics/devops/mpl/modules/Deploy/DeployWebRobocopy.groovy
+++ resources/com/griddynamics/devops/mpl/modules/Deploy/DeployWebRobocopy.groovy
@@ -1,5 +1,5 @@
 bat "net use t: /delete /y & net use T: ${CFG.'robocopy_web_destination_folder'}"
 bat "T: & cd . > app_offline.htm"
-robocopy("/MIR ${CFG.'output_directory'} ${CFG.'robocopy_web_destination_folder'} /xf app_offline.htm *.pfx")
+Robocopy("/MIR ${CFG.'output_directory'} ${CFG.'robocopy_web_destination_folder'} /xf app_offline.htm *.pfx")
 bat "T: & del app_offline.htm & net use t: /delete /y"		  
 
