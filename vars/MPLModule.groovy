c063d7e10e66e5ff4a21e86c2593e7aa91440c3f
Josh <josh.schnebly@gmail.com>
2021-01-05 13:36:50 -0600

    asdf

--- vars/MPLModule.groovy
+++ vars/MPLModule.groovy
@@ -100,7 +100,7 @@ def call(String name = env.STAGE_NAME, cfg = null) {
   def out = MPLConfig.create()
   //println "module_path-later-${module_path}"
   println "Start Module: ${module_type}-${name}"
-  //println "CFG:\r\n${Helper.formatConfig(cfg)}" 
+  println "CFG:\r\n${Helper.formatConfig(cfg)}" 
 
   if(cfg.'debug' == true) {
     MPLManager.instance.pipelineCode += "\r\n\r\n${separator}${separator}Module-${module_type}.${name}\r\n${separator}CFG:\r\n${Helper.formatConfig(cfg)}${separator}${module_src}" 
