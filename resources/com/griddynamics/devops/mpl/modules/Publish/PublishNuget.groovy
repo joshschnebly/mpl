df8f2afc09a25cd3c53f93e31376e921768bd20f
Josh <josh.schnebly@gmail.com>
2021-01-05 10:50:23 -0600

    asdf

--- resources/com/griddynamics/devops/mpl/modules/Publish/PublishNuget.groovy
+++ resources/com/griddynamics/devops/mpl/modules/Publish/PublishNuget.groovy
@@ -2,4 +2,3 @@ echo "FAKE - Publish Nuget Package"
 //withCredentials([string(credentialsId: CFG.'proget_token', variable: 'APIKey')]) {
 //    bat (label: "Publish ${CFG.'nuget_archive'} Package to Nuget", script: "dotnet nuget push ${CFG.'nuget_archive'}.nupkg -s ${CFG.'proget_server_url'} -k ${APIKey}")
 //}
-
--- resources/com/griddynamics/devops/mpl/modules/Startup/StartupSemantic.groovy
+++ resources/com/griddynamics/devops/mpl/modules/Startup/StartupSemantic.groovy
@@ -1,5 +1,3 @@
 def packageJsonPath = (CFG.'project_folder' ?: ".") + "\\package.json"
 OUT.'returns.previous_version_number' = readJSON(file: "${packageJsonPath}").version
 bat(label: 'Previous Release', script: "echo ${OUT.'returns.previous_version_number'}")
-
-
