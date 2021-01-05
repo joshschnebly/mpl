a980f4f7efe9562cbcb42232fccf7c813544c2b1
Josh <josh.schnebly@gmail.com>
2021-01-05 15:36:01 -0600

    asdf

--- resources/com/griddynamics/devops/mpl/modules/Input/Input.groovy
+++ resources/com/griddynamics/devops/mpl/modules/Input/Input.groovy
@@ -4,6 +4,6 @@ input {
       booleanParam(name:'publish_package', defaultValue: false, description: 'Publish Models Package to Nuget')
       booleanParam(name:'publish_package_beta', defaultValue: false, description: 'Publish Models Beta Package to Nuget')
     }
-  OUT.'returns.publish_package' = publish_package
-  OUT.'returns.publish_package_beta' = publish_package_beta
 }
+OUT.'returns.publish_package' = publish_package
+OUT.'returns.publish_package_beta' = publish_package_beta
