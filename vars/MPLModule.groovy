2db477bed3ff25aaf25e036f6daeae7f5aab16e7
Josh <josh.schnebly@gmail.com>
2021-01-05 13:54:09 -0600

    asdf

--- vars/MPLModule.groovy
+++ vars/MPLModule.groovy
@@ -102,7 +102,7 @@ def call(String name = env.STAGE_NAME, cfg = null) {
   println "Start Module: ${module_type}-${name}"
   println "CFG:\r\n${Helper.formatConfig(cfg)}" 
 
-  if(cfg.'debug' == 'true') {
+  if(cfg.'debug'.toBoolean()) {
     MPLManager.instance.pipelineCode += "\r\n\r\n${separator}${separator}Module-${module_type}.${name}\r\n${separator}CFG:\r\n${Helper.formatConfig(cfg)}${separator}${module_src}" 
   }
   
@@ -140,7 +140,7 @@ def call(String name = env.STAGE_NAME, cfg = null) {
   //println "end Module ${name}: ${out.toString()}"
   MPLPipelineConfigMerge(out)
 
-  if(cfg.'debug' == 'true') {
+  if(cfg.'debug'.toBoolean()) {
     MPLManager.instance.pipelineCode = Helper.formatConfig(out) == '' ? '' : "\r\n${separator}OUT: ${module_type}.${name}\r\n${Helper.formatConfig(out)}" 
   }
   //def outFormatted = Helper.formatConfig(out) == '' ? '' : "\r\n${separator}OUT: ${module_type}.${name}\r\n${Helper.formatConfig(out)}" 
