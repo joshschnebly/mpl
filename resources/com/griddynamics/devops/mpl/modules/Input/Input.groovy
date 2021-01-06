2274d396c756d4a80a159798b5f8b6ade58a4329
Josh <josh.schnebly@gmail.com>
2021-01-06 08:48:19 -0600

    asdf

--- resources/com/griddynamics/devops/mpl/modules/Input/Input.groovy
+++ resources/com/griddynamics/devops/mpl/modules/Input/Input.groovy
@@ -10,12 +10,14 @@
         //      parameters: [[$class: 'ChoiceParameterDefinition', defaultValue: 'false', description:'Publish Models Package to Nuget']
         //      ])
 //input message: 'Proceed with this step?', submitter: "${approvers}"
-
-def userInput = input message: 'User input required', parameters: [
+timeout(time: 1, unit: 'MINUTES') {
+  def userInput = input message: 'User input required', parameters: [
     booleanParam(name: 'publish_package', defaultValue: 'false', description: 'Publish Models Package to Nuget'),
-    booleanParam(name: 'publish_package_beta', defaultValue: 'false', description: 'Publish Models Beta Package to Nuget'),
-]
+    booleanParam(name: 'publish_package_beta', defaultValue: 'false', description: 'Publish Models Beta Package to Nuget')
+  ]
+  OUT.'returns.publish_package' = userInput['publish_package']
+  OUT.'returns.publish_package_beta' = userInput['publish_package_beta']
+}
 
 
-OUT.'returns.publish_package' = userInput['publish_package']
-OUT.'returns.publish_package_beta' = userInput['publish_package_beta']
+
