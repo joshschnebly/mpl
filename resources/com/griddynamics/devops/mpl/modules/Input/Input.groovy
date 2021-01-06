e1e9bb0d04f287e863e0c46fa81f715b7617c95b
Josh <josh.schnebly@gmail.com>
2021-01-06 09:14:54 -0600

    asdf

--- resources/com/griddynamics/devops/mpl/modules/Input/Input.groovy
+++ resources/com/griddynamics/devops/mpl/modules/Input/Input.groovy
@@ -1,8 +1,1 @@
-timeout(time: 1, unit: 'MINUTES') {
-  def userInput = input message: 'User input required', parameters: [
-    booleanParam(name: 'publish_package', defaultValue: 'false', description: 'Publish Models Package to Nuget'),
-    booleanParam(name: 'publish_package_beta', defaultValue: 'false', description: 'Publish Models Beta Package to Nuget')
-  ]
-  OUT.'returns.publish_package' = userInput['publish_package']
-  OUT.'returns.publish_package_beta' = userInput['publish_package_beta']
-}
+//override now - potentially parameterize later
