f59adcc468b933f414dd6d05b96f37189ae77aa3
Josh <josh.schnebly@gmail.com>
2021-01-06 09:07:24 -0600

    zxcv

--- resources/com/griddynamics/devops/mpl/modules/Input/Input.groovy
+++ resources/com/griddynamics/devops/mpl/modules/Input/Input.groovy
@@ -1,15 +1,3 @@
-        // input {
-        //   message "Input Parameters"
-        //     parameters {
-        //       booleanParam(name:'publish_package', defaultValue: false, description: 'Publish Models Package to Nuget')
-        //       booleanParam(name:'publish_package_beta', defaultValue: false, description: 'Publish Models Beta Package to Nuget')
-        //     }
-        // }
-
-        //     input(id: 'userInput', message: 'Merge to?',
-        //      parameters: [[$class: 'ChoiceParameterDefinition', defaultValue: 'false', description:'Publish Models Package to Nuget']
-        //      ])
-//input message: 'Proceed with this step?', submitter: "${approvers}"
 timeout(time: 1, unit: 'MINUTES') {
   def userInput = input message: 'User input required', parameters: [
     booleanParam(name: 'publish_package', defaultValue: 'false', description: 'Publish Models Package to Nuget'),
@@ -18,6 +6,3 @@ timeout(time: 1, unit: 'MINUTES') {
   OUT.'returns.publish_package' = userInput['publish_package']
   OUT.'returns.publish_package_beta' = userInput['publish_package_beta']
 }
-
-
-
