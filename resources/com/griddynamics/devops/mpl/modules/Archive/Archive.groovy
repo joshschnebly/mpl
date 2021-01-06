069aea22aacc29988cdbe4e7d7f17a52d1671e0d
Josh <josh.schnebly@gmail.com>
2021-01-06 09:41:28 -0600

    asdf

--- resources/com/griddynamics/devops/mpl/modules/Archive/Archive.groovy
+++ resources/com/griddynamics/devops/mpl/modules/Archive/Archive.groovy
@@ -1,1 +1,3 @@
+def archiveFilePath = "${CFG.'deployment_directory'}\\${CFG.'archive_name'}"
+if exist archiveFilePath del archiveFilePath
 zip zipFile: "${CFG.'archive_name'}", archive: true,  dir: "${CFG.'deployment_directory'}", glob: '/**/*'
