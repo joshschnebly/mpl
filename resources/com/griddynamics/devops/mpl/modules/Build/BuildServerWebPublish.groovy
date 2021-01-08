46bb12c32a66c463667d36a2f10d37370d7d5c68
Josh <josh.schnebly@gmail.com>
2021-01-08 12:23:15 -0600

    sdfg

--- resources/com/griddynamics/devops/mpl/modules/Build/BuildServerWebPublish.groovy
+++ resources/com/griddynamics/devops/mpl/modules/Build/BuildServerWebPublish.groovy
@@ -1,3 +1,2 @@
-echo "configuration_propertyWP:${CFG.'configuration_property'}"
 bat "dotnet clean ${CFG.'solution_filename'} -c ${CFG.'configuration_property'} -v minimal"
 bat "dotnet publish ${CFG.'projectfile_path'} -c ${CFG.'configuration_property'} -o ${CFG.'output_directory'} --self-contained true /p:UseAppHost=true" 
